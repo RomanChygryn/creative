e6cb8bf53a117a2364485de0f3bfdfb1bf9b0e29
RomanChygryn <r.chigrin@gmail.com>
2018-05-15 16:52:24 +0200

    some chngs

--- app/js/dropdown.js
+++ app/js/dropdown.js
@@ -1,10 +1,18 @@
-$(document).ready(function(){
-	$('.menu__toggle').click(function(){
-		$(this).toggleClass('open');
-		$('.nav__list').toggleClass('open');
-	});
+var x = document.getElementById("navbar"),
+		button = document.getElementById('nav-toggle');
 
-	$('.nav__link, .logo').click(function(){
-		$('.menu__toggle, .nav__list').removeClass('open');
-	});
-});
+
+function buttonChange() {
+button.classList.toggle("active");
+	if (x.className === "nav") {
+			x.className = "nav--mobile ";
+	} else {
+			x.className = "nav";
+	}
+};
+$(function(){
+	$('.nav__item').click(function(){
+		$('#navbar').addClass('nav').removeClass('nav--mobile');
+		$('#nav-toggle').toggleClass('active');
+	})
+})
