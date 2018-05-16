605baa0d6bc5fd7e9ce429dddf2cf1e12c3bf417
RomanChygryn <r.chigrin@gmail.com>
2018-05-16 12:46:28 +0200

    made hamburger

--- app/js/dropdown.js
+++ app/js/dropdown.js
@@ -1,18 +1,26 @@
-var x = document.getElementById("navbar"),
-		button = document.getElementById('nav-toggle');
 
-
-function buttonChange() {
-button.classList.toggle("active");
-	if (x.className === "nav") {
-			x.className = "nav--mobile ";
-	} else {
-			x.className = "nav";
-	}
-};
-$(function(){
-	$('.nav__item').click(function(){
-		$('#navbar').addClass('nav').removeClass('nav--mobile');
-		$('#nav-toggle').toggleClass('active');
+$(document).ready(function() {
+	$(function(){
+		$('#nav-toggle').click(function(){
+			$(this).toggleClass("active");
+			$('#navbar').toggleClass('nav nav--mobile');
+			$('#social__bar').toggleClass('navbar__social--mobile');
+		})
 	})
-})
+
+	$(function(){
+		$('.nav__item').click(function(){
+			$('#navbar').addClass('nav').removeClass('nav--mobile');
+			$('#nav-toggle').toggleClass('active');
+			$('#social__bar').addClass('navbar__social').removeClass('navbar__social--mobile');
+		})
+	})
+
+	$(function(){
+		$('.social__item').click(function(){
+			$('#navbar').addClass('nav').removeClass('nav--mobile');
+			$('#nav-toggle').toggleClass('active');
+			$('#social__bar').addClass('navbar__social').removeClass('navbar__social--mobile');
+		})
+	})
+});
