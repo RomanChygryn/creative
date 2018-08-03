64220155d498b62e91f649fe9bd8ccaefa1355cf
RomanChygryn <chigrin.ra@gmail.com>
2018-08-03 15:45:06 +0200

    ''

--- app/js/common.js
+++ app/js/common.js
@@ -48,3 +48,20 @@ $(document).ready(function(){
   });
 
 });
+
+
+$(document).ready(function() {
+    if(localStorage.getItem('popState') != 'shown'){
+        $("#popup").delay(2000).fadeIn();
+        localStorage.setItem('popState','shown')
+    }
+
+    $('#popup-close').click(function(e) // You are clicking the close button
+    {
+        $('#popup').fadeOut(); // Now the pop up is hiden.
+    });
+    $('#popup').click(function(e)
+    {
+        $('#popup').fadeOut();
+    });
+});
